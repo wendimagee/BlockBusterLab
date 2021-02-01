991a5a00c29a7ee5024c386b618787a6a731ab6d
Wendi Magee <wendimagee@gmail.com>
2021-02-01 14:34:08 -0500

    I have added the 2 children classes, will add the blockbuster class later

--- BlockBusterLab/Movie.cs
+++ BlockBusterLab/Movie.cs
@@ -4,43 +4,45 @@ using System.Text;
 
 namespace BlockBusterLab
 {
+    public enum Genre
+    {
+        Comedy,
+        Horror,
+        Romance,
+        Kids,
+        Action,
+        Indie,
+        Adventure,
+        Propaganda,
+        Animation
+    }
     class Movie
     {
-        public enum Genre
-        {
-            Comedy,
-            Horror,
-            Romance,
-            Kids,
-            Action,
-            Indie,
-            Adventure,
-            Propaganda,
-            Animation
-        }
-        class Movie
-        {
+
             public string Title { get; set; }
             public int RunTime { get; set; }
-            public Genre Genre { get; set; }
+            public Genre Category { get; set; }
+            public List<string> Scenes { get; set; }
+        
 
-            public Movie(string Title, int RunTime, Genre Genre)
+            public Movie(string Title, int RunTime, Genre Category, List<string> Scenes)
             {
                 this.Title = Title;
                 this.RunTime = RunTime;
-                this.Genre = Genre;
+                this.Category = Category;
+                this.Scenes = Scenes;
             }
 
-            public override string ToString()
+            public override string PrintInfo(List<Movie> movies)
+            {
+                foreach(Movie m in movies)
             {
                 string output = "";
                 output += $"Title: {Title}\n";
                 output += $"Run Time: {RunTime} Minutes\n";
-                output += $"Genre: {Genre}\n";
+                output += $"Genre: {Category}\n";
                 return output;
+            }
             }
         }
     }
-
-}
-}
---
+++ BlockBusterLab/VHS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockBusterLab
+{
+    class VHS: Movie
+    {
+        public VHS(string Title, int RunTime, Genre Category, List<string> Scenes) : base(Title, RunTime, Category, Scenes)
+        {
+
+        }
+    }
+}
