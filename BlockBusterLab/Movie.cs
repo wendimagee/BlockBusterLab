9b79bddf8812432c3d69e94dd7f6675b564ae3aa
Wendi Magee <wendimagee@gmail.com>
2021-02-01 14:18:15 -0500

    Add project files.

---
+++ BlockBusterLab/Movie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockBusterLab
+{
+    class Movie
+    {
+        public enum Genre
+        {
+            Comedy,
+            Horror,
+            Romance,
+            Kids,
+            Action,
+            Indie,
+            Adventure,
+            Propaganda,
+            Animation
+        }
+        class Movie
+        {
+            public string Title { get; set; }
+            public int RunTime { get; set; }
+            public Genre Genre { get; set; }
+
+            public Movie(string Title, int RunTime, Genre Genre)
+            {
+                this.Title = Title;
+                this.RunTime = RunTime;
+                this.Genre = Genre;
+            }
+
+            public override string ToString()
+            {
+                string output = "";
+                output += $"Title: {Title}\n";
+                output += $"Run Time: {RunTime} Minutes\n";
+                output += $"Genre: {Genre}\n";
+                return output;
+            }
+        }
+    }
+
+}
+}
---
+++ BlockBusterLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockBusterLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
