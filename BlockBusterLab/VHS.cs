bd26b237d8be0bc962f16ad4ef0f84eb84bbbaa5
Wendi Magee <wendimagee@gmail.com>
2021-02-03 11:19:20 -0500

    I only have the Rewind method left to work on, I think.

--- BlockBusterLab/VHS.cs
+++ BlockBusterLab/VHS.cs
@@ -7,72 +7,88 @@ namespace BlockBusterLab
     class VHS : Movie
     {
         public int CurrentTime { get; set; }
-        public VHS(string Title, int RunTime, Genre Category, List<string> Scenes, int currentTime=0) : base(Title, RunTime, Category, Scenes)
+        public VHS(string Title, int RunTime, Genre Category, List<string> Scenes, int CurrentTime=0) : base(Title, RunTime, Category, Scenes)
         {
-            this.CurrentTime = currentTime;
+            this.CurrentTime = CurrentTime;
         }
 
-        public static void Rewind(int currentTime)
+        public static void Rewind(int CurrentTime)
         {
-            currentTime = 0;
+            CurrentTime = 0;
         }
         public override void Play()
         {
             //A method called Play() that plays the scene at the current time and then increments CurrentTime. 
-                Console.WriteLine(Scenes[CurrentTime]);
-                if (CurrentTime < Scenes.Count + 1)
+            Console.WriteLine(Scenes[CurrentTime]);
+            if (CurrentTime +1 <= Scenes.Count)
                 {
                 CurrentTime++;
                 }
-                else
+            else if(CurrentTime + 1 > Scenes.Count)
+            {
+                Console.WriteLine("End of movie. Would you like to rewind the movie?");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "y")
                 {
-                Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
-                    string input2 = Console.ReadLine();
-                    if (input2 == "y")
-                    {
-                        Rewind(CurrentTime);
-                    }
+                    Rewind(CurrentTime);
                 }
-            
-                string input = "y";
+            }
+            else
+            {
+                Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
+                string input2 = Console.ReadLine();
+                if (input2 == "y")
+                {
+                    Rewind(CurrentTime);
+                }
+            }
+
+            string input = "y";
             while (input == "y")
             {
                 Console.WriteLine("Would you like to watch the next scene?");
                 input = Console.ReadLine().Trim().ToLower();
-                if (input == "y" || input == "yes")
-                {
-                    try
-                    {
 
-                        Console.WriteLine(Scenes[CurrentTime]);
-                        if (CurrentTime < Scenes.Count + 1)
-                        {
-                            CurrentTime++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
-                            string input3 = Console.ReadLine();
-                            if (input3 == "y")
-                            {
-                                Rewind(CurrentTime);
-                            }
-                        }
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
-                        string input3 = Console.ReadLine();
-                        if (input3 == "y")
-                        {
-                            Rewind(CurrentTime);
-                        }
-                    }
+                if (input == "y" || input == "yes")
+                { 
+                    
+                    Play(); 
                 }
 
-            }
-        
-                 }
+
+                    //    try
+                    //    {
+
+                    //        Console.WriteLine(Scenes[CurrentTime]);
+                    //        if (CurrentTime < Scenes.Count + 1)
+                    //        {
+                    //            CurrentTime++;
+                    //        }
+                    //        else
+                    //        {
+                    //            Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
+                    //            string input3 = Console.ReadLine();
+                    //            if (input3 == "y")
+                    //            {
+                    //                Rewind(CurrentTime);
+                    //            }
+                    //        }
+                    //    }
+                    //    catch
+                    //    {
+                    //        Console.WriteLine("Tape must be rewound before you can watch. Rewind now? (y/n)");
+                    //        string input3 = Console.ReadLine();
+                    //        if (input3 == "y")
+                    //        {
+                    //            Rewind(CurrentTime);
+                    //            Play();
+                    //        }
+                    //    }
+                    //}
+
+                }
+
+}
         public override string ToString()
         {
             string output = base.ToString();
